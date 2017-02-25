bc2288d80199b6108a70afd6d63babc9bc35f3df
Bryan Choe <bryan.choe88@gmail.com>
2017-02-24 15:31:53 -1000

    adds other method

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let bubbleSortGraph = document.getElementById('bubbleSort');
+/*let bubbleSortGraph = document.getElementById('bubbleSort');
 let bubbleContainer = document.createElement('div');
 bubbleContainer.style.width ='700px';
 bubbleContainer.style.height = '300px';
@@ -39,6 +39,41 @@ let bubbleFunc = function(arr){
     }
     return arr;
 };
-//console.log(bubbleFunc([5,1,4,2]));
+console.log(bubbleFunc([5,1,4,2]));*/
 
+
+/*var bubbleSort = (function(){  //use Module at the end of your var insertName
+var module = {};
+
+module.*/
+
+sort = function(array){ //<-- parameter to test in code
+var storeArray =[];
+var counter = 0;
+console.log("array",array);
+for(var r = 0; r < array.length; r++){
+   for(var i = 0; i <array.length; i++){
+    if(array[i] > array[i+1]){
+      storeArray.push(array[i]);
+      array.splice(i,1);
+      array.splice(i+1,0,storeArray[0]);
+      storeArray.pop();
+      counter++;
+    }
+  }
+}
+console.log("it takes " + counter +" passes to sort.");
+return array;
+//console.log(array);
+//console.log(array);
+return array;
+};
+
+sort([5,1,4,2,8]);
+
+console.log(sort([5,1,4,2,8]));
+
+//return module; //this returns the object module so we can access it
+//});
+//module.exports = bubbleSort; // this exports. use the
 
